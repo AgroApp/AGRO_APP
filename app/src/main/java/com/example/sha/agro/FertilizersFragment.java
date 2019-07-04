7af92f8e482e894d32ca4af0c8483e7b9fede2ac
AgroApp <agroappofficial@gmail.com>
2019-07-04 22:03:20 +0530

    database-04/07/19

--- app/src/main/java/com/example/sha/agro/FertilizersFragment.java
+++ app/src/main/java/com/example/sha/agro/FertilizersFragment.java
@@ -1,9 +1,13 @@
 package com.example.sha.agro;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +18,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -22,9 +37,14 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class FertilizersFragment extends Fragment {
 
-    //private DatabaseReference mDatabase;
     private FirebaseFirestore mDatabase;
+    private RecyclerView mMainlist;
     private View fertilizers;
+    private FertilizerFragmentAdapter listAdapter;
+    private List<FertilizersView> fertilizerViewList;
+
+
+
     public FertilizersFragment() {
         // Required empty public constructor
 
@@ -37,9 +57,33 @@ public class FertilizersFragment extends Fragment {
         super.onCreate(savedInstanceState);
         // Inflate the layout for this fragment
       fertilizers = inflater.inflate(R.layout.fragment_fertilizers, container, false);
+        fertilizerViewList = new ArrayList<>();
+        listAdapter = new FertilizerFragmentAdapter(fertilizerViewList);
+        mMainlist= (RecyclerView) fertilizers.findViewById(R.id.recycler);
+        mMainlist.setHasFixedSize(true);
+        mMainlist.setLayoutManager(new LinearLayoutManager(getContext()));
+        mMainlist.setAdapter(listAdapter);
         mDatabase = FirebaseFirestore.getInstance();
 
+      mDatabase.collection("Schemes").addSnapshotListener(new EventListener<QuerySnapshot>() {
 
+
+    @Override
+    public void onEvent( QuerySnapshot queryDocumentSnapshots,  FirebaseFirestoreException e) {
+        if(e != null){
+            Log.d(TAG,"Error : " + e.getMessage());
+        }
+
+        for (DocumentChange doc: queryDocumentSnapshots.getDocumentChanges()){
+            if(doc.getType() == DocumentChange.Type.ADDED){
+                FertilizersView fertilizerView =doc.getDocument().toObject(FertilizersView.class) ;
+                fertilizerViewList.add(fertilizerView);
+
+                listAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+});
 
         return fertilizers;
     }
---
+++ app/src/main/java/com/example/sha/agro/FertilizersView.java
@@ -0,0 +1,46 @@
+package com.example.sha.agro;
+
+public class FertilizersView {
+    String name,mobile,location,address;
+    public FertilizersView(String name, String mobile, String location, String address) {
+        this.name = name;
+        this.mobile = mobile;
+        this.location = location;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+}
